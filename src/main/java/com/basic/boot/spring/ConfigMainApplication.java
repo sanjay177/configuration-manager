5c7c9b85be45b80b18eb7f3d165d05f58775ef40
Sanjay Rawat <srawat@airbiquity.com>
2024-01-27 10:59:59 -0800

    Initial commit

--- src/main/java/com/basic/boot/spring/ConfigMainApplication.java
+++ src/main/java/com/basic/boot/spring/ConfigMainApplication.java
@@ -3,9 +3,11 @@ package com.basic.boot.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableConfigServer
 public class ConfigMainApplication {
 
 	public static void main(String[] args) {
